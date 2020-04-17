a60cfeea79e7bee67c385dc690cc9d54f38b8a24
Vitor Santos <vitors.dasilva@gmail.com>
2020-04-17 04:21:04 -0300

    Codigos do curso Ruby for Testers

---
+++ ruby/basico/loops.rb
@@ -0,0 +1,24 @@
+
+
+
+# 5.times do |i|
+#     puts 'Repetindo a mensagem ' + i.to_s + ' vez(es)'
+# end
+
+# init = 0
+# while init <= 10 do
+#     puts 'Looping infinito ' + init.to_s + ' vez(s).'
+#     init += 1
+# end
+
+# for item in (0...10)
+#     puts 'Repetindo a mensagem ' + item.to_s + 'vez(es)'
+# end
+
+#Arrays
+
+vingadores = ['Iroman', 'Hulk', 'Spiderman', 'Thor']
+
+vingadores.each do |v|
+    puts v
+end
---
+++ ruby/basico/metodos.rb
@@ -0,0 +1,22 @@
+
+
+def diga_ola(nome)
+        puts 'Olá, ' + nome
+end
+
+diga_ola('Vitor')
+
+def retorna_nome
+    return 'Vitor'
+end
+
+resultado = retorna_nome
+puts resultado
+
+# def soma(v1, v2)
+#     total = v1 + v2
+#     total
+# end
+
+# res = soma(10,25)
+# puts res
